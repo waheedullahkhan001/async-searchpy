76e694cded48ed7f783e20c0b81a0bf2cd7f9f5a
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2022-12-21 12:48:01 +0500

    Changed search method

--- async-searchpy.py
+++ async-searchpy.py
@@ -9,30 +9,39 @@ tasks = []
 results = []
 
 
-async def search(path: str, keyword: str):
+async def search(path: pathlib.Path, keyword: str, dirs: list[pathlib.Path]):
     try:
-        path = pathlib.Path(path)
-        if path.is_dir():
-            # Iterate through the directory
-            for file in path.iterdir():
-                if file.is_dir():
-                    # Create a new task for each directory and add it to the list of tasks
-                    tasks.append(asyncio.create_task(search(file, keyword)))
-                elif keyword in file.name:
-                    print(file)
-                    results.append(path)
-        elif keyword in path.name:
-            print(path)
-            results.append(path)
+        for child in path.iterdir():
+            if keyword in child.name:
+                print(child)
+                results.append(child)
+
+            if child.is_dir():
+                dirs.append(child)
     except PermissionError:
         pass
 
-async def main(path: str, keyword: str):
-    # Start the search
-    await asyncio.create_task(search(path, keyword))
-    
+async def search_all(paths: list[pathlib.Path], keyword: str) -> list[pathlib.Path]:
+    dirs = []
+
+    # Create a task for each path
+    for path in paths:
+        search_task = asyncio.create_task(search(path, keyword, dirs))
+        tasks.append(search_task)
+
     # Wait for all tasks to complete
     await asyncio.gather(*tasks)
+
+    # Clear the tasks list
+    tasks.clear()
+    
+    return dirs
+
+async def main(path: pathlib.Path, keyword: str):
+    # Start the search
+    dirs_remaining = await asyncio.create_task(search_all([path], keyword))
+    while dirs_remaining:
+        dirs_remaining = await asyncio.create_task(search_all(dirs_remaining, keyword))
 
     # Print the number of results
     print(f'Found {len(results)} results')
@@ -43,8 +52,15 @@ async def main(path: str, keyword: str):
 
 
 if __name__ == '__main__':
+    # Get the path and keyword to search for
     input_path = input('Enter path to search: ')
+    input_path = pathlib.Path(input_path)
+    while not input_path.exists() or not input_path.is_dir():
+        input_path = input('Invalid path. Enter path to search: ')
+    
     input_keyword = input('Enter keyword to search for: ')
+    while not input_keyword:
+        input_keyword = input('Invalid keyword. Enter keyword to search for: ')
 
     # Start the timer
     start_time = time.perf_counter()
